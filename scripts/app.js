8285abe6cfccaba32a413b867b211e86304cc19b
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-09 14:38:04 +0300

    add game over function

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,69 @@
 function init() {
-  const gridElem = document.querySelector(".grid")
-  const scoreELem = document.querySelector("#score-display")
-  const cells = []
-  const gridWidth = 10
-  const numberOfCells = gridWidth * gridWidth
+  const gridElem = document.querySelector(".grid");
+  const scoreELem = document.querySelector("#score-display");
+  const quackELem = document.querySelector("#quack");
+  const cells = [];
+  const gridWidth = 10;
+  const numberOfCells = gridWidth * gridWidth;
 
-  let duckPosition = 20
-  let score = 0
+  let duckPosition = 20;
+  let score = 0;
+  let totalDucks = 0;
 
   function addDuck() {
-    cells[duckPosition].classList.add("duck")
+    cells[duckPosition].classList.add("duck");
+    totalDucks++;
   }
 
   function removeDuck() {
-    cells[duckPosition].classList.remove("duck")
-  }
-  function play() {
-    setInterval(()=> {
-      removeDuck()
-      duckPosition= Math.floor(Math.random() * numberOfCells)
-      addDuck()
-    }, 3000)
+    cells[duckPosition].classList.remove("duck");
   }
 
-  function handleClick(event){
+  function endGame(){
+    alert("Game over, your score = " + score)
+    score = 0
+    scoreELem.textContent = "Your score is 0"
+    totalDucks = 0
+   
+  }
+  function play() {
+    setInterval(() => {
+      if (totalDucks < 10) {
+        removeDuck();
+        duckPosition = Math.floor(Math.random() * numberOfCells);
+        addDuck();
+      }else{
+        endGame()
+      }
+    }, 3000);
+  }
+
+  function handleClick(event) {
     // score += 10
-    if(event.target.classList.contains("duck")){
-      score += 10
-      scoreELem.textContent = `Your score is ${score}`
+    if (event.target.classList.contains("duck")) {
+      quackELem.pause();
+      quackELem.currentTime = 0;
+      score += 10;
+      scoreELem.textContent = `Your score is ${score}`;
+      quackELem.play();
     }
-    
   }
   function createGrid() {
     // for every cell, create a div
     // then append the cell to the grid
 
     for (let i = 0; i < numberOfCells; i++) {
-      const cell = document.createElement("div")
+      const cell = document.createElement("div");
       // cell.classList.add("duck")
-      cell.textContent = i
-      cell.addEventListener('click' , handleClick)
-      cells.push(cell)
-      gridElem.appendChild(cell)
+      cell.textContent = i;
+      cell.addEventListener("click", handleClick);
+      cells.push(cell);
+      gridElem.appendChild(cell);
     }
-    console.log(cells)
+    console.log(cells);
   }
 
-  createGrid()
-  play()
-
+  createGrid();
+  play();
 }
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener("DOMContentLoaded", init);
