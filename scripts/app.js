78061f2491341e023e89e7618809e155383a8693
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-09 13:32:32 +0300

    randomize and display duck

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,31 @@
 function init() {
   const gridElem = document.querySelector(".grid")
-  const cells  = []
+  const cells = []
   const gridWidth = 10
   const numberOfCells = gridWidth * gridWidth
+  let duckPosition = 20
 
+  function addDuck() {
+    cells[duckPosition].classList.add("duck")
+  }
+
+  function removeDuck() {
+    cells[duckPosition].classList.remove("duck")
+  }
+  function play() {
+    setInterval(()=> {
+      removeDuck()
+      duckPosition= Math.floor(Math.random() * numberOfCells)
+      addDuck()
+    }, 3000)
+  }
   function createGrid() {
     // for every cell, create a div
     // then append the cell to the grid
-    
+
     for (let i = 0; i < numberOfCells; i++) {
       const cell = document.createElement("div")
-      cell.classList.add("duck")
+      // cell.classList.add("duck")
       cell.textContent = i
       cells.push(cell)
       gridElem.appendChild(cell)
@@ -19,5 +34,7 @@ function init() {
   }
 
   createGrid()
+  play()
+
 }
 document.addEventListener("DOMContentLoaded", init)
