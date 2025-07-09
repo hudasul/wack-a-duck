5b077481f58147e5b4bcee4b924d72d6f662aa7f
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-09 11:41:43 +0300

    add duck class

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,21 @@
 function init() {
+  const gridElem = document.querySelector(".grid")
+  const cells  = []
+  const gridWidth = 10
+  const numberOfCells = gridWidth * gridWidth
+
   function createGrid() {
     // for every cell, create a div
     // then append the cell to the grid
-    const gridElem = document.querySelector(".grid")
-    for (let i = 0; i < 100; i++) {
+    
+    for (let i = 0; i < numberOfCells; i++) {
       const cell = document.createElement("div")
+      cell.classList.add("duck")
       cell.textContent = i
+      cells.push(cell)
       gridElem.appendChild(cell)
     }
+    console.log(cells)
   }
 
   createGrid()
