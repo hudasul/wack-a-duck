e8e23db3a9e4172f369ba25e3c9d798844ed0000
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-09 11:15:46 +0300

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,15 @@
-function init(){
-    
+function init() {
+  function createGrid() {
+    // for every cell, create a div
+    // then append the cell to the grid
+    const gridElem = document.querySelector(".grid")
+    for (let i = 0; i < 100; i++) {
+      const cell = document.createElement("div")
+      cell.textContent = i
+      gridElem.appendChild(cell)
+    }
+  }
 
-
-
-
-
-
-
-
-
+  createGrid()
 }
-document.addEventListener("DOMContentLoaded" , init)
+document.addEventListener("DOMContentLoaded", init)
